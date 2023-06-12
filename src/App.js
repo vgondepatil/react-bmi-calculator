98cd87e13e56a335a47296d1c51e17d212c4da8b
Varad Gondepatil <vgondepatil@gmail.com>
2023-06-12 16:42:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo , useState } from 'react';
+import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DEFAULT_WEIGHT = 0;
+const DEFAULT_HEIGHT = 0;
+
+export default function App() {
+  const [weight, setWeight] = useState(DEFAULT_WEIGHT)
+  const [height, setHeight] = useState(DEFAULT_HEIGHT)
+
+
+function onWeightChange(event){
+  const inputWeight = event.target.value;
+  setWeight(inputWeight)
 }
 
-export default App;
+function onHeightChange(event){ 
+  const inputHeight = event.target.value;
+  setHeight(inputHeight)
+  
+}
+
+const output = useMemo(() => {
+  const calculatedHeight = height / 100;
+  return (weight / (calculatedHeight * calculatedHeight)).toFixed(1);
+}, [weight, height]);
+
+
+  return(
+    <main>
+      <h1>BMI Calculator</h1>
+      <div className="input-selection">
+        <p className="slider-output">Weight: {weight}kg</p>
+        <input
+          className="input-slider"
+          onChange={onWeightChange}
+          type="range"
+          step="1"
+          min="40"
+          max="220"
+        />
+        <p className="slider-output">Height: {height}cm</p>
+        <input 
+        className='input-slider'
+        onChange={onHeightChange}
+        type='range'
+        step="1"
+        min="140"
+        max="240" />
+      </div>
+      <div className="output-section">
+        <p>Your BMI is</p>
+        <p className="output">{output}</p>
+      </div>
+    </main>
+  )
+
+}
